60114399713d843ff8b223157eb01768ba1c4704
Orla Madden <omadden2@gmail.com>
2020-05-13 18:52:34 +0000

    inital commit, setup flask and templates

---
+++ flaskblog.py
@@ -0,0 +1,30 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Orla Madden',
+        'title': 'Post One',
+        'content': 'First post content',
+        'date_posted': 'May 13, 2020'
+    },
+    {
+        'author': 'Vivian Leigh',
+        'title': 'Post Two',
+        'content': 'Second post content',
+        'date_posted': 'May 27, 2020'
+    }
+]
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('index.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", "5000")), debug=True) 
