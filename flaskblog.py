8d7927ea1f4a9821513774c1a76e70649261971a
Orla Madden <omadden2@gmail.com>
2020-05-15 06:22:40 +0000

    created form, form validation

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'b56362ae8d7b62f9c23b5e8047259ba6'
@@ -28,4 +29,17 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
-app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", "5000")), debug=True) 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
+app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", "5000")), debug=True)
